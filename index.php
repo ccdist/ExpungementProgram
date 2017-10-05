7e41cd967b64d6b936808797be0069c3abe8fa63
Matt Edholm <30697421+football2801@users.noreply.github.com>
2017-10-05 19:53:42 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<? require_once("_includes/header.php"); ?>
+  <title>IVYT Expungement Program</title>
+<!-- *********************************
+      INSERT PAGE SPECIFIC CSS FILE
+**************************************-->
+<link rel="stylesheet" href="_css/index.css">
+</head>
+<body>
+<? require_once("_includes/navbar.php"); ?>
+<div id="mainContainer" class="container-fluid"> <!-- gives main bg color, extends to screen width -->
+	<div id="contentContainer" class="container pt-1"> <!-- contains all content -->
+            <img class="img-fluid mx-auto d-block" src="http://lorempixel.com/960/400" alt="placeholder">
+        <div class="container">            
+            <div class="row">
+                <div class="article col-xs-12 col-sm-6 col-md-3">t commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.</div>
+                <div class="article col-xs-12 col-sm-6 col-md-3">Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.</div>
+                <div class="article col-xs-12 col-sm-6 col-md-3">aesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.</div>
+                <div class="article col-xs-12 col-sm-6 col-md-3">Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.</div>
+            </div> <!-- row -->
+            <div class="row mx-auto">
+                <div class="article col-xs-12 col-sm-6 col-md-3">Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.</div>
+                <div class="article col-xs-12 col-sm-6 col-md-3">aesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.</div>
+                <div class="article col-xs-12 col-sm-6 col-md-3">Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Curabitur blandit tempus porttitor.</div>
+            </div> <!-- row 2 -->
+        </div> <!-- row container -->
+    </div><!-- content container -->
+</div><!-- main page container -->
+
+<? require_once('_includes/bootstrapDependencies.php'); ?>
+<script src="_script/index.js"></script>
+<? require_once('_includes/footer.php'); ?>
---
+++ services.php
@@ -0,0 +1,19 @@
+<? require_once("_includes/header.php"); ?>
+  <title>IVYT Expungement Program</title>
+<!-- *********************************
+      INSERT PAGE SPECIFIC CSS FILE
+**************************************-->
+<link rel="stylesheet" href="_css/services.css">
+</head>
+<body>
+<? require_once("_includes/navbar.php"); ?>
+<div id="mainContainer" class="container-fluid"> <!-- gives main bg color, extends to screen width -->
+	<div id="contentContainer" class="container pt-1"><!-- contains all content -->
+
+
+	</div><!-- content container -->
+</div><!-- main page container -->
+
+<? require_once('_includes/bootstrapDependencies.php'); ?>
+<script src="_script/index.js"></script>
+<? require_once('_includes/footer.php'); ?>
