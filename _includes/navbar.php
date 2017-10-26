38043fa9ffeea6b54e5be43eb4cd4625700283ee
Matt Edholm <30697421+football2801@users.noreply.github.com>
2017-10-26 15:54:48 -0400

    Add files via upload

--- _includes/navbar.php
+++ _includes/navbar.php
@@ -6,14 +6,14 @@
 		</button>		
 		<div class="collapse navbar-collapse" id="navbarCollapse">
 	        <div id="navElements" class="navbar-nav mr-auto">
-	            <a id="index" href="<? echo $_SESSION['fileDirectory']; ?>index.php" class="nav-item nav-link">Home</a>
-	            <a id="services" href="<? echo $_SESSION['fileDirectory']; ?>services.php" class="nav-item nav-link">Services</a>
-	            <a id="about" href="<? echo $_SESSION['fileDirectory']; ?>about.php" class="nav-item nav-link">About</a>
-	            <a id="contact" href="<? echo $_SESSION['fileDirectory']; ?>contact.php" class="nav-item nav-link">Contact</a>
-	            <a id="mission" href="<? echo $_SESSION['fileDirectory']; ?>mission.php" class="nav-item nav-link">Mission</a>
-	            <a id="staff" href="<? echo $_SESSION['fileDirectory']; ?>staff.php" class="nav-item nav-link">Staff</a>
-	            <a id="testimonials" href="<? echo $_SESSION['fileDirectory']; ?>testimonials.php" class="nav-item nav-link">Testimonials</a>
-				<? if($_SESSION['loggedIn'] == 1){echo "<a id='adminHome' href='". $_SESSION['fileDirectory']."_protected/adminHome.php' class='nav-item nav-link'>Admin Home</a>"; } ?>
+	            <a id="index" href="<? echo $_SESSION['fileDirectory']; ?>index.php" class="nav-item nav-link"><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+	            <a id="services" href="<? echo $_SESSION['fileDirectory']; ?>services.php" class="nav-item nav-link"><i class="fa fa-list-ul" aria-hidden="true"></i> Services</a>
+	            <a id="about" href="<? echo $_SESSION['fileDirectory']; ?>about.php" class="nav-item nav-link"><i class="fa fa-id-card-o" aria-hidden="true"></i> About</a>
+	            <a id="contact" href="<? echo $_SESSION['fileDirectory']; ?>contact.php" class="nav-item nav-link"><i class="fa fa-envelope-o" aria-hidden="true"></i> Contact</a>
+	            <a id="mission" href="<? echo $_SESSION['fileDirectory']; ?>mission.php" class="nav-item nav-link"><i class="fa fa-rocket" aria-hidden="true"></i> Mission</a>
+	            <a id="staff" href="<? echo $_SESSION['fileDirectory']; ?>staff.php" class="nav-item nav-link"><i class="fa fa-users" aria-hidden="true"></i> Staff</a>
+	            <a id="testimonials" href="<? echo $_SESSION['fileDirectory']; ?>testimonials.php" class="nav-item nav-link"><i class="fa fa-comments" aria-hidden="true"></i> Testimonials</a>
+				<? if($_SESSION['loggedIn'] == 1){echo "<a id='adminHome' href='". $_SESSION['fileDirectory']."_protected/adminHome.php' class='nav-item nav-link'><i class='fa fa-unlock-alt' aria-hidden='true'></i> Admin Home</a>"; } ?>
 	            <!--<li class="nav-item dropdown">
 			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown link</a>
 			        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -23,7 +23,7 @@
 			        </div>
 			    </li>  ACTIVATE IF A DROPDOWN BOX IS REQUIRED IN THE FUTURE-->
 	        </div> <!-- navbar-nav -->
-			<? if($_SESSION['loggedIn'] == 1){echo "<a id='logout' href='". $_SESSION['fileDirectory']."logout.php' class='btn btn-outline-warning '>Logout</a>"; } ?>
+			<? if($_SESSION['loggedIn'] == 1){echo "<a id='logout' href='". $_SESSION['fileDirectory']."logout.php' class='btn btn-outline-warning '><i class='fa fa-sign-out' aria-hidden='true'></i> Logout</a>"; } ?>
 		</div><!-- navbar collapse -->
     </div> <!-- container for navbar width -->
 </nav>
