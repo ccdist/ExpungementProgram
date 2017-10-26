b8ad23bf190bf32d568490a5dcb5233442106ed1
Matt Edholm <30697421+football2801@users.noreply.github.com>
2017-10-26 12:31:12 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -9,21 +9,18 @@
 <? require_once($_SESSION['fileDirectory']."_includes/navbar.php"); ?>
 <div id="mainContainer" class="container-fluid"> <!-- gives main bg color, extends to screen width -->
 	<div id="contentContainer" class="container pt-1"><!-- contains all content -->
-
-	    <form id="login" method="post" action="_php/loginVerification.php">
+	    <form id="login" class="mx-auto pt-5 col-sm-12 col-md-4" method="post" action="_php/loginVerification.php">
 	        <fieldset>
 	            <legend>Login: </legend>
-	            <label for="login-username">Username:</label>
+	            <label for="login-username">Username:</label><br>
 	            <input id="login-username" type="text" name="username"> <br>
-	            <label for="login-password">Password:</label>
+	            <label for="login-password">Password:</label><br>
 	            <input id="login-password" type="password" name="password"> <br>
 	            <input class="buy-btn" type="submit" value="Login!">
 	            <input class="buy-btn" type="reset">
 	            <? if($_SESSION['loginError']){ ?> <span class="warning alert small"><? echo $_SESSION['loginError']; ?></span> <? } ?>
 	        </fieldset>
 	    </form>
-
-
 	</div><!-- content container -->
 </div><!-- main page container -->
 
