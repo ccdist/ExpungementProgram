cdf249396c6c648096cab3e2217f71f8701104d8
Matt Edholm <30697421+football2801@users.noreply.github.com>
2017-10-26 12:32:26 -0400

    Add files via upload

--- _protected/adminHome.php
+++ _protected/adminHome.php
@@ -13,17 +13,16 @@
 <!-- *********************************
       INSERT PAGE SPECIFIC CSS FILE
 **************************************-->
-<link rel="stylesheet" href="_css/services.css">
+<link rel="stylesheet" href="<? echo $_SESSION['fileDirectory']; ?>_css/adminHome.css">
 </head>
 <body>
 <? require_once($_SESSION['fileDirectory'] . "_includes/navbar.php"); ?>
 <div id="mainContainer" class="container-fluid"> <!-- gives main bg color, extends to screen width -->
-	<div id="contentContainer" class="container pt-1"><!-- contains all content -->
-
-
+	<div id="contentContainer" class="container py-1"><!-- contains all content -->
+		<h1>Password Protected Admin Page</h1>
+		
 	</div><!-- content container -->
 </div><!-- main page container -->
-
 <? require_once($_SESSION['fileDirectory'] . '_includes/bootstrapDependencies.php'); ?>
-<script src="<? echo $_SESSION['fileDirectory']; ?>_script/index.js"></script>
+<script src="<? echo $_SESSION['fileDirectory']; ?>_script/adminHome.js"></script>
 <? require_once($_SESSION['fileDirectory'] . '_includes/footer.php');  ?>
